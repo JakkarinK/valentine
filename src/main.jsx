c718fc77a0d96d06fbdc94929519486c7d3d1583
JAKKARINK <earth1277@gmail.com>
2026-02-14 19:59:01 +0700

    third3 commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,19 @@ import "primereact/resources/themes/lara-light-blue/theme.css";
 import { PrimeReactProvider } from "primereact/api";
 import ToastProvider from "./provider/ToastProvider.jsx";
 import { ConfirmPopup } from "primereact/confirmpopup";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <PrimeReactProvider value={{ ripple: true, unstyled: false }}>
     <ConfirmPopup />
 
     <ToastProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/valentine" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+      ,
     </ToastProvider>
   </PrimeReactProvider>,
 );
